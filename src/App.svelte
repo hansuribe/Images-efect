d6cbd69fdc0f85b43c2913548df4ac686e53aae6
Diego Enriquez-Serrano <diego@Diegos-MacBook-Pro.local>
2020-10-02 09:45:36 -0500

    dry it up

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,31 @@
 <script>
 
-	import FadeInOut from './components/Transitions/FadeInOut.svelte';
-	import Blur from './components/Transitions/Blur.svelte';
-	import Fly from './components/Transitions/Fly.svelte';
-	import Slide from './components/Transitions/Slide.svelte';
-	import Scale from './components/Transitions/Scale.svelte';
+	import Transition from './components/Transitions/Transition.svelte';
+	import { quintOut } from "svelte/easing";
+
+	export let effects = [
+		{
+		type: 'slide',
+		params: { delay: 250, duration: 1000, easing: quintOut }
+		}, {
+		type: 'scale',
+		params: { delay: 250, duration: 1000, easing: quintOut }
+		}, {
+		type: 'fly',
+		params: { delay: 150, duration: 300, x: 50, y: 0, opacity: 0.3, easing: quintOut }
+		}, {
+		type: 'fade',
+		params: { delay: 250, duration: 100 }
+		}, {
+		type: 'blur',
+		params: { amount: 100 }
+		}
+	]
 
 </script>
 
-<FadeInOut />
-
-<Blur />
-
-<Fly />
-
-<Slide />
-
-<Scale />
+{#each effects as effect}
+<p>
+  <Transition {effect} />
+</p>
+{/each}
---
+++ src/components/Transitions/Transition.svelte
@@ -0,0 +1,79 @@
+<script>
+
+  import { slide, scale, fly, fade, blur } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+
+  export let effect;
+
+  let landscapes = [
+    "https://i.postimg.cc/15ywXQZP/Dark-Souls-1.jpg",
+    "https://i.postimg.cc/Bn6F5B6V/Dark-Souls-2.jpg",
+    "https://i.postimg.cc/QxS5WyX7/Dark-Souls-3.jpg"
+  ];
+
+  let activeIndex = 0;
+
+  setInterval(() => {
+    let next = activeIndex + 1;
+    activeIndex = next == 3 ? 0 : next;
+  }, 1500);
+
+</script>
+
+<h1>{effect.type}</h1>
+
+<section>
+
+  {#each landscapes as landscape, i}
+    {#if activeIndex === i}
+      {#if effect.type == 'slide'}
+        <img
+          transition:slide={effect.params}
+          src={landscape}
+          width="1280"
+          height="720"
+          alt="dark souls landscape {effect.type}" />
+      {:else if effect.type == 'scale'}
+        <img
+          transition:scale={effect.params}
+          src={landscape}
+          width="1280"
+          height="720"
+          alt="dark souls landscape" />
+      {:else if effect.type == 'fly'}
+        <img
+          transition:fly={effect.params}
+          src={landscape}
+          width="1280"
+          height="720"
+          alt="dark souls landscape" />
+      {:else if effect.type == 'fade'}
+        <img
+          transition:fade={effect.params}
+          src={landscape}
+          width="1280"
+          height="720"
+          alt="dark souls landscape" />
+      {:else if effect.type == 'blur'}
+        <img
+          transition:blur={effect.params}
+          src={landscape}
+          width="1280"
+          height="720"
+          alt="dark souls landscape" />
+      {/if}
+    {/if}
+  {/each}
+
+</section>
+
+<style>
+  
+  section {
+    display: flex;
+    justify-content: center;
+    height: 720px;
+  }
+
+  section img {position: absolute}
+</style>
